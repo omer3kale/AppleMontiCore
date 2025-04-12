63c6332d59c8eab6d1bb034629bb2d908743322c
Ömer Ückale <ueckale@se-rwth.de>
2025-04-13 00:02:25 +0200

    Update HTMLExporter.java

--- HTMLExporter.java
+++ HTMLExporter.java
@@ -1,4 +1,10 @@
+package de.yourdomain.frontend;
+
+import de.monticore.ui._ast.*;
+import java.util.stream.Collectors;
+
 public class HTMLExporter {
+
     public static String generateHTML(ASTUI ui) {
         StringBuilder out = new StringBuilder();
         for (ASTUIElement element : ui.getUIElementList()) {
@@ -14,13 +20,18 @@ public class HTMLExporter {
                     .map(HTMLExporter::toHTML)
                     .collect(Collectors.joining()) +
                 "</div>\n";
+
         } else if (element instanceof ASTText) {
-            return "<p>" + ((ASTText) element).getSTRING() + "</p>\n";
+            return "<p>" + ((ASTText) element).getSTRING().replace("\"", "") + "</p>\n";
+
         } else if (element instanceof ASTButton) {
             ASTButton btn = (ASTButton) element;
-            String onclick = btn.isPresentAction() ? " onclick=\"" + btn.getAction().getSTRING() + "\"" : "";
-            return "<button" + onclick + ">" + btn.getSTRING() + "</button>\n";
+            String onclick = btn.isPresentAction() ? 
+                " onclick=\"" + btn.getAction().getSTRING().replace("\"", "") + "\"" : "";
+            return "<button" + onclick + ">" + btn.getSTRING().replace("\"", "") + "</button>\n";
         }
-        return "";
+
+        return "<!-- Unknown element -->\n";
     }
-}
+} 
+
