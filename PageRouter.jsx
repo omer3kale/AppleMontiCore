8b40164c97a6252dd3d833fe511c92cf4ad95d74
Ömer Ückale <ueckale@se-rwth.de>
2025-04-13 01:58:48 +0200

    Update PageRouter.jsx

--- PageRouter.jsx
+++ PageRouter.jsx
@@ -4,6 +4,7 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom';
 import HomePage from './HomePage';
 import ContactPage from './ContactPage';
 import WelcomeBanner from './WelcomeBanner';
+import NotFound from './NotFound';
 
 export default function PageRouter() {
   return (
@@ -13,6 +14,7 @@ export default function PageRouter() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/welcome" element={<WelcomeBanner name="Guest" />} />
+        <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
