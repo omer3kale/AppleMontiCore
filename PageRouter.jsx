9c3a0d7cbed7509b2b18ebfbda5a21c761b247e9
Ömer Ückale <ueckale@se-rwth.de>
2025-04-13 01:57:46 +0200

    Create PageRouter.jsx

---
+++ PageRouter.jsx
@@ -0,0 +1,19 @@
+import React from 'react';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+
+import HomePage from './HomePage';
+import ContactPage from './ContactPage';
+import WelcomeBanner from './WelcomeBanner';
+
+export default function PageRouter() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<HomePage />} />
+        <Route path="/home" element={<HomePage />} />
+        <Route path="/contact" element={<ContactPage />} />
+        <Route path="/welcome" element={<WelcomeBanner name="Guest" />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
