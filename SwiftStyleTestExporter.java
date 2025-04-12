736ddf323504fdd4ade3315f2fe51c8d24654261
Ömer Ückale <ueckale@se-rwth.de>
2025-04-13 00:22:56 +0200

    Update SwiftStyleTestExporter.java

--- SwiftStyleTestExporter.java
+++ SwiftStyleTestExporter.java
@@ -28,6 +28,27 @@ public class SwiftStyleTestExporter {
         return sb.toString();
     }
 
+    public static String exportSwiftTest(ASTVisitorTestFile ast) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("import XCTest\n\n");
+        sb.append("final class VisitorTests: XCTestCase {\n\n");
+
+        for (ASTVisitorTest test : ast.getVisitorTestList()) {
+            String type = test.getTypeName();
+            sb.append("  func test").append(type).append("Visit() {\n");
+            sb.append("    let node = /* AST").append(type).append(" instance */\n");
+
+            for (ASTAssertion assertion : test.getAssertionList()) {
+                sb.append("    ").append(toSwiftAssertion(assertion)).append("\n");
+            }
+
+            sb.append("  }\n\n");
+        }
+
+        sb.append("}\n");
+        return sb.toString();
+    }
+
     private static String toJavaAssertion(ASTAssertion a) {
         if (a instanceof ASTEquals) {
             ASTEquals eq = (ASTEquals) a;
@@ -35,6 +56,17 @@ public class SwiftStyleTestExporter {
         } else if (a instanceof ASTContains) {
             ASTContains c = (ASTContains) a;
             return "assertTrue(node.get" + capitalize(c.getFieldRef().getFieldName()) + "().contains(\"" + c.getSTRING() + "\"));";
+        }
+        return "// unsupported assertion";
+    }
+
+    private static String toSwiftAssertion(ASTAssertion a) {
+        if (a instanceof ASTEquals) {
+            ASTEquals eq = (ASTEquals) a;
+            return "XCTAssertEqual(node." + eq.getFieldRef().getFieldName() + ", \"" + eq.getSTRING() + "\")";
+        } else if (a instanceof ASTContains) {
+            ASTContains c = (ASTContains) a;
+            return "XCTAssertTrue(node." + c.getFieldRef().getFieldName() + ".contains(\"" + c.getSTRING() + "\"))";
         }
         return "// unsupported assertion";
     }
