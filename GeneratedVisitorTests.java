d504e17043d6ac27440fdd48bfcceb052f6a594c
Ömer Ückale <ueckale@se-rwth.de>
2025-04-13 02:28:50 +0200

    Update GeneratedVisitorTests.java

--- GeneratedVisitorTests.java
+++ GeneratedVisitorTests.java
@@ -1,11 +1,21 @@
-@Test
-public void testTextVisit() {
-  ASTText node = ...;
-  assertEquals("Welcome", node.getSTRING());
-}
+import de.monticore.viewdsl._ast.*;
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
 
-@Test
-public void testButtonVisit() {
-  ASTButton node = ...;
-  assertTrue(node.getSTRING().contains("Click"));
+public class GeneratedVisitorTests {
+
+  @Test
+  public void testTextVisit() {
+    ASTText node = new ASTText();
+    node.setSTRING("Welcome");
+    assertEquals("Welcome", node.getSTRING());
+  }
+
+  @Test
+  public void testButtonVisit() {
+    ASTButton node = new ASTButton();
+    node.setSTRING("Click here to start");
+    assertTrue(node.getSTRING().contains("Click"));
+  }
+
 }
