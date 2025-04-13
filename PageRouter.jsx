3d4e91b5fa5bd555fc5cb3c601b81fe4b10f1394
Ömer Ückale <ueckale@se-rwth.de>
2025-04-13 02:46:39 +0200

    Update PageRouter.jsx

--- PageRouter.jsx
+++ PageRouter.jsx
@@ -5,6 +5,7 @@ import HomePage from './HomePage';
 import ContactPage from './ContactPage';
 import WelcomeBanner from './WelcomeBanner';
 import NotFound from './NotFound';
+import FootballAutomatonGame from './components/FootballAutomatonGame';
 
 export default function PageRouter() {
   return (
@@ -14,6 +15,7 @@ export default function PageRouter() {
         <Route path="/home" element={<HomePage />} />
         <Route path="/contact" element={<ContactPage />} />
         <Route path="/welcome" element={<WelcomeBanner name="Guest" />} />
+        <Route path="/game" element={<FootballAutomatonGame />} />
         <Route path="*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
